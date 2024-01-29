62e17222e47800f7cb354f384cf067296bb51d6d
slatejack <1815846446@qq.com>
2024-01-29 14:40:22 +0800

    feat: init project

---
+++ config/webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const ESLintWebpackPlugin = require('eslint-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+const sassRegex = /\.(scss|sass)$/;
+const sassModuleRegex = /\.module\.(scss|sass)$/;
+const resolve = filePath => path.join(__dirname, filePath);
+const isDev = process.env.NODE_ENV === 'development';
+
+module.exports = {
+  entry: './app.ts',
+  devtool: isDev ? 'eval-cheap-source-map' : 'hidden-source-map',
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        parallel: 4,
+        terserOptions: {
+          compress: {
+            ecma: 5,
+            drop_console: !isDev,
+          },
+        },
+      }),
+    ]
+  },
+  output: {
+    path: resolve('../dist/'),
+    filename: '[name].js',
+    chunkFilename: '[name].chunk.js',
+    library: {
+      type: 'commonjs-static',
+    },
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.scss', '.css'],
+    alias: {
+      '@': resolve('../src'),
+    },
+  },
+  plugins: [new ESLintWebpackPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.ts[x]?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ]
+  },
+};
